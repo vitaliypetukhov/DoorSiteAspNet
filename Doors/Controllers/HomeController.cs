79cd76be21df3ea830fb3ecb75d64decce64c42c
Виталий <skaleckiy.vitalik.1994@gmail.com>
2020-05-18 18:33:26 +0300

    Добавлена HomePage, топ 3 популярных дверей и карусель картинок на главную страницу

---
+++ Doors/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doors.Data.Interfaces;
+using Doors.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doors.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDoorRepository _doorRepository;
+        public HomeController(IDoorRepository doorRepository)
+        {
+            _doorRepository = doorRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PreferredDoors = _doorRepository.PreferredDoors
+            };
+
+            return View(homeViewModel);
+        }
+    } 
+}
---
+++ Doors/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Doors.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doors.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable <Door> PreferredDoors { get; set; }
+    }
+}
