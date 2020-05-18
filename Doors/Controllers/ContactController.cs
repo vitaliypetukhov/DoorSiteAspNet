8784e997aa272f16980de90dd959b374be1e56b4
Виталий <skaleckiy.vitalik.1994@gmail.com>
2020-05-18 18:48:38 +0300

    Добавлен EmailTagHelper

---
+++ Doors/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doors.Data.Interfaces;
+using Doors.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doors.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    } 
+}
