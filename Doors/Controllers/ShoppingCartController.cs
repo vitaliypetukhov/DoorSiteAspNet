ebef4a99ee55d72356d20510b08d474abf86f0b9
Виталий <skaleckiy.vitalik.1994@gmail.com>
2020-05-18 13:26:50 +0300

    Добавлен ShoppingCartController, а так же его View

---
+++ Doors/Controllers/ShoppingCartController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doors.Data.Interfaces;
+using Doors.Data.Models;
+using Doors.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Doors.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IDoorRepository _doorRepository;
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartController(IDoorRepository doorRepository, ShoppingCart shoppingCart)
+        {
+            _doorRepository = doorRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+
+            var sCVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(sCVM); 
+        }
+
+        public RedirectToActionResult AddToShoppingCart(int doorId)
+        {
+            var selectedDoor = _doorRepository.Doors.FirstOrDefault(p => p.DoorId == doorId);
+            if (selectedDoor != null)
+            {
+                _shoppingCart.AddToCart(selectedDoor, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int doorId)
+        {
+            var selectedDoor = _doorRepository.Doors.FirstOrDefault(p => p.DoorId == doorId);
+            if (selectedDoor != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedDoor);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
