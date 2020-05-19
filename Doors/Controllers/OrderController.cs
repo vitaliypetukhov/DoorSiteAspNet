ed7c3dd5a66659ccf934d98cd925a0c7e2ea308c
Виталий <skaleckiy.vitalik.1994@gmail.com>
2020-05-19 23:51:26 +0300

    Сделана валидация при заказе товара

--- Doors/Controllers/OrderController.cs
+++ Doors/Controllers/OrderController.cs
@@ -21,13 +21,19 @@ namespace Doors.Controllers
             _shoppingCart = shoppingCart;
         }
 
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Checkout(Order order)
         {
             var items = _shoppingCart.GetShoppingCartItems();
             _shoppingCart.ShoppingCartItems = items;
             if (_shoppingCart.ShoppingCartItems.Count == 0)
             {
-                ModelState.AddModelError("", "Ваша корзина пуста, добавте для начала товар");
+                ModelState.AddModelError("", "Ваша корзина пуста, добавьте для начала товар");
             }
 
             if (ModelState.IsValid)
